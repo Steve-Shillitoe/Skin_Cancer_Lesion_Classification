419f05776bff75734aa8752988bb61595982d4ba
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2023-09-26 02:38:35 +0100

    improved comments

--- Skin_Cancer_Lesion_Classification.py
+++ Skin_Cancer_Lesion_Classification.py
@@ -18,24 +18,24 @@ Dermatofibroma (df)
 
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-import os
-from glob import glob
-import seaborn as sns
-from PIL import Image
+#import pandas as pd
+# import os
+# from glob import glob
+# import seaborn as sns
+# from PIL import Image
 
 
 np.random.seed(42)
-from sklearn.metrics import confusion_matrix
+#from sklearn.metrics import confusion_matrix
 
 import keras
-from keras.utils import to_categorical # used for converting labels to one-hot-encoding
+#from keras.utils import to_categorical # used for converting labels to one-hot-encoding
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D, BatchNormalization
-from sklearn.model_selection import train_test_split
-from scipy import stats
-from sklearn.preprocessing import LabelEncoder
-from sklearn.utils import resample
+#from sklearn.model_selection import train_test_split
+#from scipy import stats
+#from sklearn.preprocessing import LabelEncoder
+#from sklearn.utils import resample
 from keras.preprocessing.image import ImageDataGenerator
 
 def gallery_show(images):
@@ -46,8 +46,6 @@ def gallery_show(images):
         plt.axis("off")
     plt.show()
     #plt.savefig('NoisyImage.jpg')
-
-skin_df = pd.read_csv('data/HAM10000_metadata.csv')
 
 
 SIZE = 32
